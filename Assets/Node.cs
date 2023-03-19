f16723d54d360c6016f03376dc8a631188fa152f
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2023-03-19 00:42:24 -0700

    implemented node-based movement system

---
+++ Assets/Node.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject upNode, downNode, leftNode, rightNode;
+    enum TileType {
+        emptyTile,
+        wallTile,
+        foodTile
+    };
+    [SerializeField]
+    private TileType tileType;
+    private void Start() {
+        if (tileType != TileType.wallTile && (upNode == null || downNode == null || leftNode == null || rightNode == null)) {
+            //Debug.LogError("Error: Nodes require a connection in all directions unless they are a wall");
+        }
+    }
+    public GameObject getNode(int direction) {
+        switch (direction) {
+            case 0:
+                return upNode;
+            case 1:
+                return downNode;
+            case 2:
+                return leftNode;
+            case 3:
+                return rightNode;
+            default:
+                return null;
+        }
+    }
+    public bool isWall() {
+        return tileType == TileType.wallTile;
+    }
+
+
+    //for setting up colliders
+    /*
+    private void OnTriggerEnter2D(Collider2D collision) {
+        string dir = collision.name;
+        switch (dir) {
+            case "Down":
+                upNode = collision.transform.parent.gameObject;
+                break;
+            case "Up":
+                downNode = collision.transform.parent.gameObject;
+                break;
+            case "Right":
+                leftNode = collision.transform.parent.gameObject;
+                break;
+            case "Left":
+                rightNode = collision.transform.parent.gameObject;
+                break;
+        }
+    } */
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,23 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     [SerializeField]
     private float TILE_SIZE = 1.5f
                 , MOVE_SPEED = 1;
     [SerializeField]
     private Rigidbody2D RB;
-
-    void Update()
-    {
+    [SerializeField]
+    private GameObject nextObject;
+    private Node nextNode;
+    private Vector3 nextPosition;
+    private Vector3 nextPositionNormalized;
+    [SerializeField]
+    private int nextDirection = (int)directions.left;
+    private enum directions {
+        noDir = -1,
+        up,
+        down,
+        left,
+        right
+    }
+    private void Start() {
+        nextNode = nextObject.GetComponent<Node>();
+        nextPosition = nextObject.transform.position;
+        nextPositionNormalized = (transform.position - nextPosition).normalized;
+    }
+    void Update() {
         if (Input.GetKeyDown("w"))
-            RB.velocity = Vector2.up * MOVE_SPEED;
+            nextDirection = (int) directions.up;
         if (Input.GetKeyDown("s"))
-            RB.velocity = Vector2.down * MOVE_SPEED;
+            nextDirection = (int) directions.down;
         if (Input.GetKeyDown("d"))
-            RB.velocity = Vector2.right * MOVE_SPEED;
+            nextDirection = (int) directions.right;
         if (Input.GetKeyDown("a"))
-            RB.velocity = Vector2.left * MOVE_SPEED;
+            nextDirection = (int) directions.left;
+
+        if (nextDirection == (int) directions.noDir) {
+            //stay still
+        }
+        else if (Vector3.Distance(transform.position, nextPosition) > MOVE_SPEED * 0.01) {
+            transform.position = transform.position - nextPositionNormalized * MOVE_SPEED * Time.deltaTime;
+        } else {
+            transform.position = nextPosition;
+            GameObject tempObject = nextNode.getNode(nextDirection);
+            nextNode = tempObject.GetComponent<Node>();
+            if (nextNode.isWall())
+                lose();
+            else {
+                nextPosition = tempObject.transform.position;
+                nextPositionNormalized = (transform.position - nextPosition).normalized;
+            }
+        }
+    }
+    private void lose() {
+        Debug.Log("you died");
+        nextDirection = (int) directions.noDir;
+        //death noise
+        //death animation
+        //restart level quickly
     }
 }
